42f1c4fa68228d864ef3fd31b31df69e58a800d9
Andrey Bulanov <bulanov1804@gmail.com>
2021-09-04 00:01:55 +0300

    add Statistics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
 
 import Profile from './components/Profile/Profil.jsx';
+import Statistics from './components/Statistics/Statistics.jsx';
 
 export default function App() {
   return (
@@ -15,9 +17,9 @@ export default function App() {
         />
       }
 
-      {/* <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
-      <FriendList friends={friends} />
+      {/* <FriendList friends={friends} />
       <TransactionHistory items={transactions} /> */}
     </div>
   );
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+
+export default function Statistics({ title, stats }) {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+      <ul className={s.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            className={s.item}
+            key={id}
+            style={{ backgroundColor: randomColor() }}
+          >
+            <span className={s.label}>{label}</span>
+            <span className={s.percentage}>{percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    }),
+  ),
+};
+
+function randomColor() {
+  const color = Math.floor(Math.random() * 16777216).toString(16);
+  return '#000000'.slice(0, -color.length) + color;
+}

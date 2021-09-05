4b79d53a95058f980621b363f437ed510fb60b84
Andrey Bulanov <bulanov1804@gmail.com>
2021-09-05 20:59:10 +0300

    Add TransactionsHistiory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import user from './data/user.json';
 import statisticalData from './data/statistical-data.json';
 import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 import Profile from './components/Profile/Profil.jsx';
 import Statistics from './components/Statistics/Statistics.jsx';
 import FriendList from './components/FriendList/FriendList.jsx';
+import TransactionsHistory from './components/TransactionsHistory/TransactionsHistory.jsx';
 
 export default function App() {
   return (
@@ -22,7 +24,7 @@ export default function App() {
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
       <FriendList friends={friends} />
-      {/* <TransactionHistory items={transactions} /> */}
+      <TransactionsHistory items={transactions} />
     </div>
   );
 }
